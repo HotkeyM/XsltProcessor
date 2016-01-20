38553cad9f887c5b18d1d02afd808af78ef95354
Utyamishev <hotkeym@gmail.com>
2016-01-20 18:33:46 +0300

    XsltProcessor
    Libxml 2, MsXml and Saxon (throw) JNI realizations

---
+++ xsltprocessor.cpp
@@ -0,0 +1,65 @@
+#include "xsltprocessor.h"
+#include <QFile>
+#include <QTextStream>
+
+XsltProcessor::XsltProcessor(QObject *parent) : QObject(parent)
+{
+
+}
+
+QString XsltProcessor::ProcessXsltFiles(const QString &xmlFile, const QString &xsltFile)
+{
+    QFile xml(xmlFile);
+    xml.open(QIODevice::ReadOnly);
+    QString xmlData = xml.readAll();
+    xml.close();
+
+    QFile xslt(xsltFile);
+    xml.open(QIODevice::ReadOnly);
+    QString xsltData = xslt.readAll();
+    xslt.close();
+
+    return ProcessXslt(xmlData, xsltData);
+
+}
+
+XsltProcessor::~XsltProcessor()
+{
+
+}
+
+bool XsltProcessor::ProcessXsltToFile(const QString &xmlData, const QString &xsltData, const QString &dstFile)
+{
+    QFile f(dstFile);
+    if (!f.open(QIODevice::WriteOnly)) return false;
+    QTextStream str(&f);
+    QString data = ProcessXslt(xmlData,xsltData);
+    if (data.isEmpty())
+    {
+        f.close();
+        return false;
+    }
+    str << data;
+    f.close();
+
+    return true;
+}
+
+bool XsltProcessor::ProcessXsltFilesToFile(const QString &xmlFile, const QString &xsltFile, const QString &dstFile)
+{
+
+    QFile f(dstFile);
+    if (!f.open(QIODevice::WriteOnly)) return false;
+    QTextStream str(&f);
+    QString data = ProcessXsltFiles(xmlFile,xsltFile);
+    if (data.isEmpty())
+    {
+        f.close();
+        return false;
+    }
+    str << data;
+    f.close();
+
+    return true;
+}
+
